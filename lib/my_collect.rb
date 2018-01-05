853e441268f7f56d2f03bc265b250955166cf16b
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-05 17:04:57 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,7 +1,12 @@
 def my_collect(array)
   if block_given?
-    puts "given"
-  elsif
+    i = 0
+    new_array = []
+    while i < array.count
+      yield(array[i])
+      i += 1
+    end
+    elsif
     puts "not given"
   end
 end
